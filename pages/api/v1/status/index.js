a4948cd6c9b565587d10a5e367e770112ccc897d
thsc47 <thsc47@gmail.com>
2024-01-01 14:17:47 -0300

    feat: add `version_version` to status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,17 @@
+import database from "infra/database";
+
 async function status(req, res) {
   const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
   res.status(200).json({
     updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,11 +1,23 @@
 test("get to /api/v1/status should return 200 ", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+});
+
+test("get to /api/v1/status should return a correct value to updated_at ", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  console.log(responseBody.updated_at);
   expect(responseBody.updated_at).toBeDefined();
 
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+});
+
+test("get to /api/v1/status should return a correct value to postgres_version ", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.dependencies.database.version).toEqual(`16.0`);
 });
