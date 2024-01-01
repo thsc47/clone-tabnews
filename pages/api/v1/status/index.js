a94a1190d7ecfe15603561bd683f6105286284bc
thsc47 <thsc47@gmail.com>
2024-01-01 14:24:54 -0300

    refactor: change `database.js` to allways close conections

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -5,11 +5,25 @@ async function status(req, res) {
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const dataBaseOpenConnectionsValue = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity where datname = 'local_db';",
+  );
+
+  console.log(dataBaseOpenConnectionsValue.rows[0]);
+
   res.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: dataBaseOpenConnectionsValue.rows.length,
       },
     },
   });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -5,7 +5,6 @@ test("get to /api/v1/status should return 200 ", async () => {
 
 test("get to /api/v1/status should return a correct value to updated_at ", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
 
   const responseBody = await response.json();
   expect(responseBody.updated_at).toBeDefined();
@@ -16,8 +15,23 @@ test("get to /api/v1/status should return a correct value to updated_at ", async
 
 test("get to /api/v1/status should return a correct value to postgres_version ", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
 
   const responseBody = await response.json();
   expect(responseBody.dependencies.database.version).toEqual(`16.0`);
+});
+
+test("get to /api/v1/status should return a correct value to max_connections ", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+});
+
+test("get to /api/v1/status should return a correct value to open_connections ", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json();
+  console.log(responseBody.dependencies.database.opened_connections);
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
 });
