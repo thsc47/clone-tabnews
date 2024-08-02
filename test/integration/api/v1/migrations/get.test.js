e903ae1b2b72fea0e80e4c512cd70383d2568a28
Thiago Campos <thsc47@gmail.com>
2024-08-02 15:42:24 +0000

    change get tests

--- test/integration/api/v1/migrations/get.test.js
+++ test/integration/api/v1/migrations/get.test.js
@@ -2,7 +2,7 @@ import database from "infra/database";
 
 beforeAll(ClearDatabase);
 
-test("GET to /api/v1/migrations should return 200", async () => {
+test("GET: to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
