7ccb9f458ee7b1ffab6fe2ce3b6bf3bab1035018
Thiago Campos <thsc47@gmail.com>
2024-11-20 15:52:20 +0000

    test: rename all tests

--- test/integration/api/v1/migrations/post.test.js
+++ test/integration/api/v1/migrations/post.test.js
@@ -1,30 +1,41 @@
-import database from "infra/database";
-import orchestrator from "test/orchestrator.js";
+import orchestrator from "test/orchestrator";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
+  await orchestrator.clearDatabase();
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const beforeResponse = await fetch(
-    "http://localhost:3000/api/v1/migrations",
-    {
-      method: "POST",
-    },
-  );
-  expect(beforeResponse.status).toBe(201);
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response1.status).toBe(201);
 
-  const responseBody = await beforeResponse.json();
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+        const response1Body = await response1.json();
 
-  const afterResponse = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+        expect(Array.isArray(response1Body)).toBe(true);
+        expect(response1Body.length).toBeGreaterThan(0);
+      });
+      test("For the second time", async () => {
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response2.status).toBe(200);
+
+        const response2Body = await response2.json();
+
+        expect(Array.isArray(response2Body)).toBe(true);
+        expect(response2Body.length).toBe(0);
+      });
+    });
   });
-  expect(afterResponse.status).toBe(200);
-
-  const emptyBody = await afterResponse.json();
-  expect(Array.isArray(emptyBody)).toBe(true);
-  expect(emptyBody.length).toBe(0);
 });
